391a972c5c36beeb2dcee6b84e7f150f1927456c
veronica-repo <jacob7432@gmail.com>
2024-02-16 16:16:05 +0900

    Veronica: Test

--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
+import os
+
+import sys
+
 import os
 import sys
 
@@ -18,6 +22,6 @@ def main():
     sys.path.append('/home/ubuntu/ccs-kaist/')
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     main()
+
